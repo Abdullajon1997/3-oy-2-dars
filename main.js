856ce582e4b2b516583d04a60e5c5587bda21d1c
Abdullajon1997 <abdullajonibrohimov0@gmail.com>
2022-09-16 17:44:24 +0300

    2-vazifa

---
+++ main.js
@@ -0,0 +1,481 @@
+// ====================Mantqiy amallar ==================//
+
+// 1-misol
+
+// function boolen (a)
+// {
+//     if (a>0)
+//     {
+//         console.log("a son musbat");
+//     }
+//     else
+//     {
+//         console.log("a son manfiy");
+//     }
+// }boolen(25)
+
+// 2-misol va 3-misol
+// function son (a)
+// {
+//     if (a%2==0)
+//     {
+//         console.log("a juft son");
+//     }
+//     else
+//     {
+//         console.log("a toq son");
+//     }
+// }son(27)
+
+// 4-misol
+// function son (a,b)
+// {
+//     if(a>0 && b>0)
+//     {
+//         console.log("a va b sonlar musbat");
+//     }
+//     if(a<0 && b>0)
+//     {
+//         console.log("a son manfiy b son musbat");
+//     } 
+//     if(a>0 && b<0)
+//     {
+//         console.log("a son musbat b son manfiy");
+//     }
+//     else
+//     if(a<0 && b<0)
+//     {
+//         console.log("a va b sonlar manfiy");
+//     }
+// }son(-45,55)
+
+// 5-misol va 6-misol
+// function son (a,b,c)
+// {
+//     if (a>b && b>c)
+//     {
+//         console.log("true");
+//     }
+//     else
+//     {
+//         console.log("false");
+//     }
+// }son(9,7,2)
+
+// 7-misol
+
+// function son (a,b)
+// {
+//     if (a%2==1 && b%2==1)
+//     {
+//         console.log("a va b toq sonlar");
+//     }
+//     else
+//     {
+//         console.log("E'tiborli bo'ling");
+//     }
+// }son (25,97)
+
+// 8-misol
+
+// function son (a,b,c)
+// {
+//     if (a>0 && b>0 && c>0)
+//     {
+//         console.log("Barchasi musbat");
+//     }
+//     else
+//     {
+//         console.log("E'tiborli bo'ling");
+//     }
+// }son(25,52,87)
+
+// 9-misol
+
+// function son (a,b,c)
+// {
+//     if(a>0 && b>0 && c<0)
+//     {
+//         console.log("a va b sonlar musbat c son manfiy");
+//     }
+//     if(a>0 && b<0 && c>0)
+//     {
+//         console.log("a va c sonlar musbat b son manfiy");
+//     }
+//     else
+//     if(a<0 && b>0 && c>0)
+//     {
+//         console.log("b va c sonlar musbat a son manfiy");
+//     }
+// }son(45,-23,55)
+
+// 10-misol
+
+// function hafta (a)
+// {
+//     if(a===1)
+// {
+//     console.log("Dushanba");
+// }
+// else 
+// if(a===2)
+// {
+//     console.log("Seshanba");
+// }
+// else
+// if(a===3)
+// {
+//     console.log("Chorshanba");
+// }
+// else
+// if(a===4)
+// {
+//     console.log("Payshanba");
+// }
+// else
+// if(a===5)
+// {
+//     console.log("Juma");
+// }
+// else
+// if(a===6)
+// {
+//     console.log("Shanba");
+// }
+// else
+// if(a===7)
+// {
+//     console.log("Yakshanba");
+// }
+// else
+// {
+//     console.log("Unday hafta kuni mavjud emas");
+// }
+// }hafta(2)
+
+// 11-misol
+
+// function kod (a)
+// {
+//     if(a==90 || a==91)
+//     {
+//         console.log("Beeline UZ");
+//     }
+//     if(a==93 || a==94)
+//     {
+//         console.log("Ucell");
+//     }
+//     else
+//     if(a==88)
+//     {
+//         console.log("Mobi.uz");
+//     }
+// }kod(90)
+
+// 12-misol
+
+// function son (a)
+// {
+//     if(a>0)
+//     {
+//         console.log(a+1);
+//     }
+//     else
+//     if (a<0)
+//     {
+//         console.log(a-1);
+//     }
+// }son(-45)
+
+// 13-misol
+
+// function son (a)
+// {
+//     if(a>0)
+//     {
+//         console.log(a+3);
+//     }
+//     else
+//     if (a<0)
+//     {
+//         console.log(a-2);
+//     }
+// }son(45)
+
+// 14-misol
+
+// function son (a,b)
+// {
+//     if (a>b)
+//     {
+//         console.log("a b dan katta");
+//     }
+//     if(a<b)
+//     {
+//         console.log("b a dan katta");
+//     }
+//     else
+//     if(a==b)
+//     {
+//         console.log("a va b teng kuchga teng");
+//     }
+// }son(7,9)
+
+// 15-misol
+
+// function son (a,b,c)
+// {
+//     if (a<b>c)
+//     {
+//         console.log(b + " Kiritgan qiymatlaringiz orasida katta");
+//     }
+//     if (a>b>c)
+//     {
+//         console.log(a + " Kiritgan qiymatlaringiz orasida katta");
+//     }
+//     if(a<b<c)
+//     {
+//         console.log(c + " Kiritgan qiymatlaringiz orasida katta");
+//     }
+
+// }son(16,13,17)
+
+// 16-misol
+
+// function son (a,b,c)
+// {
+//     if (a<b<c)
+//     {
+//         console.log(a + " Kiritgan qiymatlaringiz orasida eng kichigi");
+//     }
+        // else
+//     if(a>b && b<c)
+//     {
+//         console.log(b + " Kiritgan qiymatlaringiz orasida eng kichigi");
+//     }
+//     else
+//     {
+//         console.log(c + " Kiritgan qiymatlaringiz orasida eng kichigi");
+//     }
+
+// }son(10,9,1)
+
+// 17-misol
+
+// const odam=document.querySelector(".person")
+// let jins="ayol";
+// let s=""
+// if(jins=="erkak") {
+//      s=`<img src="./img/boy.jpg"/>`
+// }else{
+//     s=`<img src="./img/girl.webp" />`
+// }
+// odam.innerHTML=s
+
+// 18-misol
+
+// function baxo (a)
+// {
+//     if(a==1 || a==2)
+//     {
+//         console.log("stipendiyangiz 6-oyga kuydi");
+//     }
+//     else
+//     if(a===3)
+//     {
+//         console.log("300 000 so'm");
+//     }
+//     else
+//     if(a===4)
+//     {
+//         console.log("475 000 so'm");
+//     }
+//     else
+//     if(a===5)
+//     {
+//         console.log("517 000 so'm");
+//     }
+//     else
+//     {
+//         console.log("Bo'ldi endi inson o'ziga yarashgan yarashgan ishni qilish kerak");
+//     }
+// }baxo(7)
+
+// 19-misol
+
+//  function son (a)
+//  {
+//     if(a>0 && a%2==0)
+//     {
+//         console.log("Juft musbat");
+//     }
+//     else
+//     if(a<0 && a%2==0)
+//     {
+//         console.log("Juft manfiy");
+//     }
+//     else
+//     if(a>0 && a%2==1)
+//     {
+//         console.log("Toq musbat");
+//     }
+//     else
+//     {
+//         console.log("Toq manfiy");
+//     }
+//  }son(-5)
+
+// 20-misol
+
+// function yosh (a)
+// {
+//     if(a<2022)
+//     {
+//         console.log(2022-a);
+//     }
+//     else
+//     if(a>2022)
+//     {
+//         console.log("Hali dunyoga kelmagan");
+//     }
+//     else
+//     {
+//         console.log("Minus qiymat qabul qilmaydi !!!");
+//     }
+// }yosh(1997)
+
+// 21-misol
+
+// function son (a,b,c)
+// {
+//     if(a<=b<=c)
+//     {
+//         console.log(true);
+//     }
+// }son(55,56,57)
+
+// 22-misol
+
+// function son (a,b)
+// {
+//     if(a%2==1 && b%2==0 || a%2==0 && b%2==1)
+//     {
+//         console.log("Faqat bittasi toq son");
+//     }
+//     else
+//     {
+//         console.log("Mantiqqa to'g'ri kelmaydi");
+//     }
+// }son(21,30)
+
+// 23-misol
+
+// function son (a,b,c)
+// {
+//     if(a>0 && b>0 && c>0)
+//     {
+//         console.log("Har biri musbat son");
+//     }
+//     else
+//     {
+//         console.log("Xato !!!");
+//     }
+// }son(1,2,5)
+
+// 24-misol
+
+// function son (a,b,c)
+// {
+//     if(a>0 || b>0 || c>0)
+//     {
+//         console.log("Berilgan sonlarni biri musbat son");
+//     }
+//     else
+//     {
+//         console.log("Xatolik");
+//     }
+// }son(-2,5,-7)
+
+// 25-misol
+
+// function son (a)
+// {
+//     let yuzlik,onlik;
+//     yuzlik=Math.trunc(a/100);
+//     console.log(yuzlik);
+//     onlik=Math.trunc(a/10%10);    
+//     console.log(onlik);
+//     birlik=Math.trunc(a%10);
+//     console.log(birlik);
+//     if(yuzlik!==onlik && onlik!==birlik && yuzlik!==birlik)
+//     {
+//         console.log("Raqamlar bir xil emas");
+//     }
+//     else
+//     {
+//         console.log("Raqamlar orasida bir xilliklar mavjud");
+//     }
+// }son(457)
+
+// 26-misol
+
+// function son (a)
+// {
+//     let yuzlik,onlik;
+//     yuzlik=Math.trunc(a/100);
+//     console.log(yuzlik);
+//     onlik=Math.trunc(a/10%10);    
+//     console.log(onlik);
+//     birlik=Math.trunc(a%10);
+//     console.log(birlik);
+//     if(birlik<onlik || onlik<yuzlik)
+//     {
+//         console.log("O'sib borish taribida joylashgan");
+//     }
+//     else
+//     {
+//         console.log("Xatolik mavjud !!!");
+//     }
+// }son(347)
+
+// 27-misol
+
+// function son (a)
+// {
+//     if(a===hp)
+//     {
+//         console.log("7 200 000 so'm");
+//     }
+//     else
+//     if(a===asus)
+//     {
+//         console.log("6 899 000 so'm");
+//     }
+//     else
+//     if(a===lenova)
+//     {
+//         console.log("5 555 000 so'm");
+//     }
+//     else
+//     {
+//         console.log("Ma'lumotlar bazasidan topilmadi !!!");
+//     }
+// }son(lenova)
+
+// 28-miosl
+
+// function son (a)
+// {
+//     let s=0;
+//     for(let i=s; s<a;i++)
+//     {
+//         s++;
+//         console.log(s);
+//     }
+// }son(15)
+
+
+
+
+
+
